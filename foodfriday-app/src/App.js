aa740f4e7fbd7952a02f5b8f35c8296286bccaf1
Diego <diego@Diego-Chavez-Worths-MacBook-Pro.local>
2018-08-20 16:53:45 +0200

    initial commit with router

--- foodfriday-app/src/App.js
+++ foodfriday-app/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Router, Link } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Screens/HomeScreen';
+
+let Dash = () => <div>Dash</div>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Home path="/" />
+          <Dash path="dashboard" />
+        </Router>
       </div>
     );
   }
