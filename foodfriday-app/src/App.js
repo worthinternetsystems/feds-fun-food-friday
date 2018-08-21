b6f3bd8767e441f5620f704f2e3b856b16122ba3
Chris Johnson <cjohnson@worthit.nl>
2018-08-21 12:36:59 +0200

    Remove Base Landing Page
    Remove Unused classes

--- foodfriday-app/src/App.js
+++ foodfriday-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './ffflogo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Food Fun Friday App</h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
