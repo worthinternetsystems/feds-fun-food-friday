896e5494c66011a2e2b02190b5fae6a3d6010e22
Elliot Goldsmith <egoldsmith@worth.systems>
2018-08-21 16:52:00 +0200

    init

---
+++ foodfriday-app/src/Screens/RestaurantsList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+let data = [
+  {
+    title: '',
+    description: '',
+    category: ''
+  },
+  {
+    title: '',
+    description: '',
+    category: ''
+  },
+  {
+    title: '',
+    description: '',
+    category: ''
+  },
+];
+
+class RestaurantsList extends Component {
+
+
+
+  return (
+
+  );
+}
+
+export default RestaurantsList;
