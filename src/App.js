02b61d082980ce1f9ebb11e3f8be64043066bac9
Elvis Furcic <furcicelvis@gmail.com>
2018-08-22 16:38:35 +0200

    Merge branch 'develop' of https://github.com/worthinternetsystems/feds-fun-food-friday into feature/change-structure
    # Conflicts:
    #	foodfriday-app/src/App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Router} from '@reach/router';
 import './App.css';
 import Home from './Screens/HomeScreen';
+import RestaurantsList from './Screens/RestaurantsList';
 
 let Dash = () => <div>Dash</div>;
 let ChooseUserType = () => <div>Choose Your Adventure</div>
@@ -14,6 +15,7 @@ class App extends Component {
                     <Home path="/"/>
                     <ChooseUserType path="/chooseuserrole"/>
                     <Dash path="dashboard"/>
+                    <RestaurantsList path="/restaurants-list"/>
                 </Router>
             </div>
         );
